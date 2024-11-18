1e29df1559c31bbf672f65df11fb1e58a4c19b2c
AMGewka <devid.gabibov@gmail.com>
2024-11-18 20:15:01 +0500

    update 1.8.6
    Обновление кода:
    - Обновлен API кассы
    - В создание новой транзакции добавлено описание покупки (Buying products: {products} on {username} account)
    - В создание новой транзакции добавлен email покупателя
    - Добавлено поле для ввода резервного email (На случай если покупку делает незарегистрированный на сайте игрок, будет использовать резеврный email)
    - Добавлен украинский язык в меню настройки кассы
    - Добавлена генерации ссылок "URL Оповещения" и "URL успешной оплаты"
    - Обновлен вид настройки кассы и ее описание
    
    Обновление кассы:
    - Добавлены новые методы оплаты: Hamster Kombat и Карты Узбекистана
    - Новый API

---
+++ Store_1.8+/modules/Store/gateways/AAIO/gateway.php
@@ -0,0 +1,151 @@
+<?php
+/**
+ * AAIO_Gateway class
+ *
+ * @package Modules\Store
+ * @author AMGewka
+ * @version 1.8.6
+ * @license MIT
+ */
+class AAIO_Gateway extends GatewayBase
+{
+	public function __construct()
+	{
+		$name = 'AAIO';
+		$author = '<a href="https://github.com/AMGewka" target="_blank">AMGewka</a>';
+		$gateway_version = '1.8.6';
+		$store_version = '1.7.1';
+		$settings = ROOT_PATH . '/modules/Store/gateways/AAIO/gateway_settings/settings.php';
+
+		parent::__construct($name, $author, $gateway_version, $store_version, $settings);
+	}
+
+	public function onCheckoutPageLoad(TemplateBase $template, Customer $customer) : void {}
+
+	public function processOrder(Order $order) : void
+	{
+		$merchant_id = StoreConfig::get('AAIO.shopid_key');
+		$amount = $order->getAmount()->getTotalCents() / 100;
+		$currency = $order->getAmount()->getCurrency();
+		$secret = StoreConfig::get('AAIO.secret1_key');
+		$order_id = $order->data()->id;
+		$referral = '434b7ffe10d8';
+		$desc = 'Buying products: ' . $order->getDescription() . ' on ' . $order->customer()->getUser()->data()->username . ' account';
+		$email = $order->customer()->getUser()->data()->email;
+
+		if (empty($email)) {
+			$email = StoreConfig::get('AAIO.admin_email');
+		}
+		$sign = hash('sha256', implode(':', [$merchant_id, $amount, $currency, $secret, $order_id]));
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, 'https://aaio.so/merchant/get_pay_url');
+		curl_setopt(
+			$ch,
+			CURLOPT_HTTPHEADER,
+			[
+				'Accept: application/json'
+			]
+		);
+		curl_setopt(
+			$ch,
+			CURLOPT_POSTFIELDS,
+			http_build_query(
+				[
+					'merchant_id' => $merchant_id,
+					'amount' => $amount,
+					'currency' => $currency,
+					'order_id' => $order_id,
+					'email' => $email,
+					'desc' => $desc,
+					'referral' => $referral,
+					'sign' => $sign,
+				]
+			)
+		);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 15);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 60);
+
+		$result = curl_exec($ch);
+		$http_code = curl_getinfo($ch, CURLINFO_RESPONSE_CODE);
+
+		if (curl_errno($ch)) {
+			die('Connect error:' . curl_error($ch));
+		}
+		curl_close($ch);
+
+		if (!in_array($http_code, [200, 400, 401])) {
+			die('Response code: ' . $http_code);
+		}
+
+		$decoded = json_decode($result, true);
+
+		if (json_last_error() !== JSON_ERROR_NONE) {
+			die('Error: Failed to parse response');
+		}
+
+		if ($decoded['type'] == 'success') {
+			if (isset($decoded['url'])) {
+				header("Location: " . $decoded['url']);
+				exit;
+			} else {
+				die('Error: Payment URL not found');
+			}
+		} else {
+			die('Error: ' . $decoded['message']);
+		}
+	}
+
+	public function handleReturn() : bool
+	{
+		if (isset($decoded['url'])) {
+			header("Location: " . $decoded['url']);
+			exit;
+		}
+
+		return false;
+	}
+
+	public function handleListener() : void
+	{
+		$shopId = StoreConfig::get('AAIO.shopid_key');
+		$apiKey = StoreConfig::get('AAIO.secret2_key');
+
+		$allowedIps = array(
+			'91.107.204.74',
+		);
+
+		if (!in_array($_SERVER['REMOTE_ADDR'], $allowedIps)) {
+			die("Error: Untrusted IP address");
+		}
+
+		$data = $_POST;
+
+		$signString = $data['merchant_id'] . ':' . $data['amount'] . ':' . $data['currency'] . ':' . $apiKey . ':' . $data['order_id'];
+		$sign = hash('sha256', $signString);
+
+		if ($sign != $data['sign']) {
+			die("Error: Invalid signature");
+		}
+
+		$paymentId = $data['order_id'];
+		$orderAmount = $data['amount'];
+		$currency = $data['currency'];
+
+		$payment = new Payment($paymentId, 'transaction');
+		$paymentData = [
+			'order_id' => $paymentId,
+			'gateway_id' => $this->getId(),
+			'transaction' => $paymentId,
+			'amount_cents' => Store::toCents($orderAmount),
+			'currency' => $currency,
+			'fee_cents' => '0'
+		];
+		$payment->handlePaymentEvent(Payment::COMPLETED, $paymentData);
+	}
+}
+
+$gateway = new AAIO_Gateway();
---
+++ Store_1.8+/modules/Store/gateways/AAIO/gateway_settings/settings.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+ *  Made by AMGewka
+ *  https://github.com/AMGewka
+ *
+ *  License: MIT
+ *
+ *  Store module
+ */
+require_once(ROOT_PATH . '/modules/Store/classes/StoreConfig.php');
+$aaio_language = new Language(ROOT_PATH . '/modules/Store/gateways/AAIO/language', LANGUAGE);
+$page_title = $aaio_language->get('gateways', 'aaio');
+
+if (Input::exists()) {
+	if (Token::check()) {
+		if (isset($_POST['shopid_key']) && isset($_POST['secret1_key']) && isset($_POST['secret2_key']) && isset($_POST['admin_email']) && strlen($_POST['shopid_key']) && strlen($_POST['secret1_key']) && strlen($_POST['secret2_key']) && strlen($_POST['admin_email'])) {
+			StoreConfig::set('AAIO.shopid_key', $_POST['shopid_key']);
+			StoreConfig::set('AAIO.secret1_key', $_POST['secret1_key']);
+			StoreConfig::set('AAIO.secret2_key', $_POST['secret2_key']);
+			StoreConfig::set('AAIO.admin_email', $_POST['admin_email']);
+		}
+
+        // Is this gateway enabled
+		if (isset($_POST['enable']) && $_POST['enable'] == 'on') $enabled = 1;
+		else $enabled = 0;
+
+		DB::getInstance()->update(
+			'store_gateways',
+			$gateway->getId(),
+			[
+				'enabled' => $enabled
+			]
+		);
+
+		Session::flash('gateways_success', $language->get('admin', 'successfully_updated'));
+	} else $errors = [$language->get('general', 'invalid_token')];
+}
+
+$smarty->assign(
+	[
+		'SETTINGS_TEMPLATE' => ROOT_PATH . '/modules/Store/gateways/AAIO/gateway_settings/settings.tpl',
+		'ENABLE_VALUE' => ((isset($enabled)) ? $enabled : $gateway->isEnabled()),
+		'SHOP_ID_VALUE' => ((isset($_POST['shopid_key']) && $_POST['shopid_key']) ? Output::getClean(Input::get('shopid_key')) : StoreConfig::get('AAIO.shopid_key')),
+		'SHOP_API_KEY_VALUE' => ((isset($_POST['secret1_key']) && $_POST['secret1_key']) ? Output::getClean(Input::get('secret1_key')) : StoreConfig::get('AAIO.secret1_key')),
+		'SHOP_API_KEY_2_VALUE' => ((isset($_POST['secret2_key']) && $_POST['secret2_key']) ? Output::getClean(Input::get('secret2_key')) : StoreConfig::get('AAIO.secret2_key')),
+		'ADMIN_EMAIL_VALUE' => ((isset($_POST['admin_email']) && $_POST['admin_email']) ? Output::getClean(Input::get('admin_email')) : StoreConfig::get('AAIO.admin_email')),
+		'ADMIN_EMAIL' => $aaio_language->get('adminemail'),
+		'SHOP_ID' => $aaio_language->get('shopid'),
+		'SHOP_KEY1' => $aaio_language->get('key1'),
+		'SHOP_KEY2' => $aaio_language->get('key2'),
+		'ENABLE_GATEWAY' => $aaio_language->get('enablegateway'),
+		'GATEWAY_NAME' => $aaio_language->get('gatewayname'),
+		'BANK_CARD' => $aaio_language->get('bankcard'),
+		'ONLINE_PAYMENTS' => $aaio_language->get('onlinepay'),
+		'ONLINE_WALLET' => $aaio_language->get('onlinewal'),
+		'CRYPTOCURRENCIES' => $aaio_language->get('crypto'),
+		'GATEWAY_LINK' => $aaio_language->get('gatewaylink'),
+		'GATEWAY_TESTED' => $aaio_language->get('gatewaytest'),
+		'ALERT_URL' => $aaio_language->get('alerturl'),
+		'SUCCESS_URL' => $aaio_language->get('sucurl'),
+		'PINGBACK_URL' => rtrim(URL::getSelfURL(), '/') . URL::build('/store/listener', 'gateway=AAIO'),
+		'SUCC_URL' => rtrim(URL::getSelfURL(), '/') . URL::build('/store/checkout', 'do=complete'),
+		'FAILED_URL' => $aaio_language->get('failurl'),
+		'WARINFO1' => $aaio_language->get('warinfo1'),
+		'WARINFO2' => $aaio_language->get('warinfo2')
+	]
+);
